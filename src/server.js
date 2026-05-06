2f79609a0730f77589a6ec76e530f69de5481f98
Humainlink <kumak6213@gmail.com>
2026-05-06 21:24:15 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
+const express = require('express');
 const admin = require('firebase-admin');
 const serviceAccount = require('/etc/secrets/firebase.json');
+
+const app = express();
+const PORT = process.env.PORT || 10000;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 console.log('Firebase Admin initialisé');
+
+// Route de test pour Render
+app.get('/', (req, res) => {
+  res.send('Serveur Alerte Gouv OK');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
