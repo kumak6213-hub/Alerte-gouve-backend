1a66984ab1acf26cbb110b647b6e18f49d5bcae2
Humainlink <kumak6213@gmail.com>
2026-05-06 19:44:51 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    privateKey: process.env.FIREBASE_PRIVATE_KEY-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgk...\\n-----END PRIVATE KEY-----\\n
   })
 });
 
